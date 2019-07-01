d6101481c526bc12281d97185cac547b87d1d607
unknown <connelallison@hotmail.com>
2019-07-01 11:15:53 +0100

    make changes based on test case results, and add comments for time/space complexity

--- rotate_array.js
+++ rotate_array.js
@@ -1,19 +1,25 @@
 const rotateArray = function (array, rotation) {
-  const rotations = rotation % array.length;
+  let rotations = rotation % array.length;
+  if (rotations < 0) {
+    rotations = array.length + rotations;
+  }
   const rotatedArray = [];
   for (let i = 0; i < array.length; i++) {
     if (i + rotations < array.length) {
-      rotatedArray[i] = array[i + rotations];
+      rotatedArray[i + rotations ] = array[i];
     } else {
-      rotatedArray[i] = array[i + rotations - array.length];
+      rotatedArray[i + rotations - array.length] = array[i];
     }
   }
   return rotatedArray;
 }
 
-const array1 = [1, 2, 3, 4, 5, 6];
-console.log(rotateArray(array1, 3));
-console.log(rotateArray(array1, 7));
-console.log(rotateArray(array1, -2));
-console.log(rotateArray(array1, 6));
-console.log(rotateArray(array1, 6000));
+// const array1 = [1, 2, 3, 4, 5, 6];
+// console.log(rotateArray(array1, 3));
+// console.log(rotateArray(array1, 7));
+// console.log(rotateArray(array1, -2));
+// console.log(rotateArray(array1, 6));
+// console.log(rotateArray(array1, 6000));
+// console.log(rotateArray(array1, -555));
+
+// This function is O(n) for run time and O(n) for memory.
