a5023927af3c5f9a6716169ef3126f661bb3a130
unknown <connelallison@hotmail.com>
2019-07-01 11:08:11 +0100

    add test cases

--- rotate_array.js
+++ rotate_array.js
@@ -1,12 +1,19 @@
 const rotateArray = function (array, rotation) {
-  const rotation = rotation % array.length;
+  const rotations = rotation % array.length;
   const rotatedArray = [];
   for (let i = 0; i < array.length; i++) {
-    if (i + rotation < array.length) {
-      rotatedArray[i] = array[i + rotation];
+    if (i + rotations < array.length) {
+      rotatedArray[i] = array[i + rotations];
     } else {
-      rotatedArray[i] = array[i + rotation - array.length];
+      rotatedArray[i] = array[i + rotations - array.length];
     }
   }
   return rotatedArray;
 }
+
+const array1 = [1, 2, 3, 4, 5, 6];
+console.log(rotateArray(array1, 3));
+console.log(rotateArray(array1, 7));
+console.log(rotateArray(array1, -2));
+console.log(rotateArray(array1, 6));
+console.log(rotateArray(array1, 6000));
